81305652cb8778fe997d10ce7631d88737b0f3c3
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-22 17:06:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const increment = document.getElementById("counter");
 const button = document.getElementById("incrementBtn");
 let counter=0;
 const listener= function(){
+	alert(counter);
 	counter++;
-	if(counter===3){
-		alert(+counter);
+	/*if(counter===3){
+		alert(counter);
 		button.removeEventListener("click",listener);
-	}
+	}*/
 }
 button.addEventListener("click", listener);
 
