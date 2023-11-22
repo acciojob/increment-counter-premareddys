9622dc069af63e2e49e0cf696faa21b06bcafd04
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-11-22 17:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
-const increment = document.getElementById("counter");
+const totalCount = document.getElementById("counter");
 const button = document.getElementById("incrementBtn");
-let counter=0;
+let count=0;
 const listener= function(){
-	alert(counter);
-	counter++;
+	totalCount.innerHTML = count;
+	count++;
 	/*if(counter===3){
 		alert(counter);
 		button.removeEventListener("click",listener);
